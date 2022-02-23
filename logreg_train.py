80edbaac2bd69686b15edc7bbb658bc3ffd05fc6
jnolleau <j.nolleau88@gmail.com>
2022-02-23 20:21:58 +0100

    regression working with accuracy 99%

--- logreg_train.py
+++ logreg_train.py
@@ -68,20 +68,23 @@ def log_reg_skl(df_train, df_test):
 
     clf = LogisticRegression()
     clf.fit(X_train, y_train)
-    print(clf.coef_)
-    print(clf.n_iter_)
+    print('weights', clf.coef_)
+    print('bias', clf.intercept_)
     y_pred = clf.predict(X_test)
     pd.DataFrame(y_pred).to_csv(
         'houses.csv', header=[target], index_label='Index')
     y_true = pd.read_csv('houses_true.csv', sep=',', index_col="Index")
+
+    # count nbr of true / false
+    diff = (y_pred == y_true['Hogwarts House']).value_counts()
+    print(diff)
+
+    # Accuracy score
     score = accuracy_score(y_true=y_true, y_pred=y_pred) * 100
     print(f"accuracy_score: {score}%")
 
 
 # 1. model F
-def model(X, W):
-    return X.dot(W)
-
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
@@ -105,11 +108,8 @@ def cost_function(y, H0):
     m = len(y)
     return -1/m * np.sum(y * np.log(H0) + (1 - y) * np.log(1 - H0))
 
-# def log_loss(y, A):
-#   return 1/len(y) * np.sum(-y * np.log(A) - (1 - y) * np.log(1 - A))
 
 # 3. Gradient descent
-
 
 def grad(X, y, H0):
     m = len(y)
@@ -118,17 +118,36 @@ def grad(X, y, H0):
     return dW, db
 
 
-def gradient_descent(X, y, H0, W, b, learningRate):
-    dW, db = grad(X, y, H0)
-    W = W - learningRate * dW
-    b = b - learningRate * db
-    return W, b
+def gradient_descent(X, y, W, b, learningRate):
+    n_iteration = 150
+    cost_history = np.zeros((n_iteration, 1))
+    for i in range(n_iteration):
+        H0 = H0_calculation(X, W, b)
+        dW, db = grad(X, y, H0)
+        W = W - learningRate * dW
+        b = b - learningRate * db
+        cost_history[i] = cost_function(y, H0)
+    return W, b, cost_history
 
+# Prediction 
+def predict(X, W_all, b_all, classes):
+    H0 = pd.DataFrame()
+    for W, b in zip(W_all, b_all.T):
+        W = W.reshape(W.shape[0], 1) 
+        H0_class = H0_calculation(X, W, b)
+        H0 = pd.concat([H0, H0_class], axis=1)
+    H0.columns = classes
+    y_pred = H0.idxmax(axis=1)
+    pd.DataFrame(y_pred).to_csv(
+        'houses.csv', header=['House'], index_label='Index')
+    
+    y_true = pd.read_csv('houses_true.csv', sep=',', index_col="Index")
+    score = accuracy_score(y_true=y_true, y_pred=y_pred) * 100
+    diff = (y_pred == y_true['Hogwarts House']).value_counts()
+    print(diff)
+    return y_pred, score
 
-def log_reg(df_train, df_test):
-    features = ['Defense Against the Dark Arts',
-                'Herbology', 'Ancient Runes', 'Charms']
-    target = 'Hogwarts House'
+def log_reg(df_train, df_test, features, target):
 
     # Get the mean by feature
     mean = df_train[features].mean()
@@ -146,23 +165,30 @@ def log_reg(df_train, df_test):
     classes = y_train.unique()
     nb_of_class = classes.shape[0]
     house_labels = {house: i for house, i in zip(classes, range(nb_of_class))}
-    y = y_train.replace(house_labels)
-    y = y.values.reshape(y.shape[0], 1)
-    W_all = np.zeros((nb_of_class, X_train.shape[1]))
-    epoch = 500
-    cost_history = []
+    print(house_labels)
+    y_train = y_train.replace(house_labels)
+    y_train = y_train.values.reshape(y_train.shape[0], 1)
+    W = np.zeros((nb_of_class, X_train.shape[1]))
+    b = np.zeros((1, nb_of_class))
+    cost_history = np.zeros((1, 1))
+    # Wb_dict = {f'W{i}': [] for i in range(nb_of_class)}
+    # Wb_dict['b'] = []
+    
     for curr_class in range(nb_of_class):
-        W, b = initialisation(X_train)
-        y_class = y
-        y_class[y_class != curr_class] = 0
-        y_class[y_class == curr_class] = 1
-        for i in range(0, epoch):
-            H0 = H0_calculation(X_train, W, b)
-            W, b = gradient_descent(X_train, y_class, H0, W, b, learningRate=0.05)
-            cost_history.append(cost_function(y_class, H0))
-        W_all[curr_class] = W.T
-    print(W_all)
-    return cost_history
+        W_class, b_class = initialisation(X_train)
+        y = np.where(y_train == curr_class, 1, 0)
+        W_class, b_class, cost_history_class = gradient_descent(X_train, y, W_class, b_class, learningRate=0.1)
+        W[curr_class] = W_class.T
+        b[0:, curr_class] = b_class
+        cost_history = np.concatenate((cost_history, cost_history_class), axis=0)
+        # Wb_dict[f'W{curr_class}'].append(W_class.T)
+        # Wb_dict['b'].append(b_class[0])
+
+    y_pred, score = predict(X_test, W, b, classes)
+    print(y_pred)
+    print(f"accuracy_score: {score}%")
+
+    return W, b, cost_history
 
 
 if (len(sys.argv) > 1):
@@ -171,7 +197,20 @@ if (len(sys.argv) > 1):
     if (args):
         df_train = read_csv(args[0], index_col="Index")
         df_test = read_csv("datasets/dataset_test.csv", index_col="Index")
-        # cost_history = log_reg(df_train, df_test)
+        
+        features = ['Defense Against the Dark Arts',
+                'Herbology', 'Ancient Runes', 'Charms']
+        target = 'Hogwarts House'
+        
+        W, b, cost_history = log_reg(df_train, df_test, features, target)
+        
+        # Create a dataframe with W and b to store it nicely
+        Wb_dict = {f'W{i}': W[i] for i in range(W.shape[1])}
+        Wb_dict['b'] = b[0]
+        weights = pd.DataFrame(Wb_dict, index=df_train[target].unique())
+        weights.to_csv('weights.csv')
+        print(weights)
+        # print(weights.loc['W0'])
         # plt.figure(figsize=(9, 6))
         # plt.plot(cost_history)
         # plt.xlabel('n_iteration')
@@ -180,7 +219,7 @@ if (len(sys.argv) > 1):
         # plt.show()
 
         # print()
-        log_reg_skl(df_train, df_test)
+        # log_reg_skl(df_train, df_test)
     else:
         print("Bad file extension")
         sys.exit(1)
